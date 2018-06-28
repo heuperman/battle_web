f8b013cf47270b982ce16c22bff214df76074471
Cornelis Heuperman <cheuperman@gmail.com>
2018-06-28 16:12:35 +0100

    Separate post and get for entry of names

--- app.rb
+++ app.rb
@@ -1,20 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
-   erb :players
+    erb :players
   end
 
-post '/names' do
-  @player1 = params[:player1]
-  @player2 = params[:player2]
-  erb :names
-end
+  post '/enter_names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/names'
+  end
 
-
-
-
-
+  get '/names' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb :names
+  end
 
   run! if app_file == $0
 end
