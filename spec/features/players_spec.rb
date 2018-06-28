491cabccdb47ac02644af990204bbe6a395d6aa7
Yvonne Tang <ytang604@gmail.com>
2018-06-28 16:38:55 +0100

    viewing hit points

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -8,3 +8,13 @@ feature 'Entering player names' do
     expect(page).to have_content "Kees and Yvonne start the battle"
   end
 end
+
+feature 'View hits points' do
+  scenario 'see Player2 hit points' do
+    visit('/')
+    fill_in :player1, with: 'Kees'
+    fill_in :player2, with: 'Yvonne'
+    click_button 'Submit'
+    expect(page).to have_content 'Yvonne: 60HP'
+ end
+end
